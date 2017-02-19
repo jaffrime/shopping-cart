15d3c81e8229d89c74c4181b6889b5191e90d512
Julian Affrime <julian.affrime@gmail.com>
2017-02-19 13:13:29 +0200

    added page, need to connect to updated items

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // an array with all of our cart items
 var cart = [];
+var addedObjects = [{item: 'keys', price: 20, url: 'http://www.gerritbrands.com/sites/default/files/post-images/keys.jpg'}];
 
 var updateCart = function () {
   // TODO: finish
@@ -43,5 +44,26 @@ $('.clear-cart').on('click', function () {
   clearCart();
 });
 
+var addItemFunc = function () {
+  var item_name = $('#item-name').val();
+  var item_price = $('#item-price').val();
+  var item_url = $('#item-url').val();
+
+  addedObjects.push({
+    "item": item_name,
+    "price": item_price,
+    "url": item_url
+  });
+};
+
+var updateStore = function () {
+  for (i=0; i<addedObjects.length; i++) {
+    $('.new-row').append (
+        '<div class="col-md-4"> <div class="card-container"> <div class="card item" item data-name="'+addedObjects[i].item+'" data-price="'+addedObjects[i].price+'"> <div class="pricebox"> <p class="price"> $'+addedObjects[i].price+' </p> </div> <div class="buybox"> <p class="add-to-cart"> ADD TO CART </p> </div> <div class="card-inner"> <img src="'+addedObjects[i].url+'" class="proimage"> </div> </div> </div> </div>'
+    )
+  }
+};
+
 // update the cart as soon as the page loads!
 updateCart();
+updateStore();
