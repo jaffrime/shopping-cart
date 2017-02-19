71140975bf9e96d202c74a458155ba3855bb7a73
Julian Affrime <julian.affrime@gmail.com>
2017-02-19 13:39:21 +0200

    function works, need to go back to one page

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // an array with all of our cart items
 var cart = [];
-var addedObjects = [{item: 'keys', price: 20, url: 'http://www.gerritbrands.com/sites/default/files/post-images/keys.jpg'}];
+// var addedObjects = [{item: 'keys', price: 20, url: 'http://www.gerritbrands.com/sites/default/files/post-images/keys.jpg'}];
 
 var updateCart = function () {
   // TODO: finish
@@ -49,21 +49,37 @@ var addItemFunc = function () {
   var item_price = $('#item-price').val();
   var item_url = $('#item-url').val();
 
-  addedObjects.push({
-    "item": item_name,
-    "price": item_price,
-    "url": item_url
-  });
+  app.addObject(item_name, item_price, item_url);
 };
 
-var updateStore = function () {
-  for (i=0; i<addedObjects.length; i++) {
-    $('.new-row').append (
+var RenderStore = function () {
+  var addedObjects = [];
+
+  var addObject = function (item, price, url) {
+    addedObjects.push({
+      "item": item,
+      "price": price,
+      "url": url
+    });
+  };
+
+  var updateStore = function () {
+    for (i=0; i<addedObjects.length; i++) {
+      $('.new-row').append (
         '<div class="col-md-4"> <div class="card-container"> <div class="card item" item data-name="'+addedObjects[i].item+'" data-price="'+addedObjects[i].price+'"> <div class="pricebox"> <p class="price"> $'+addedObjects[i].price+' </p> </div> <div class="buybox"> <p class="add-to-cart"> ADD TO CART </p> </div> <div class="card-inner"> <img src="'+addedObjects[i].url+'" class="proimage"> </div> </div> </div> </div>'
-    )
-  }
+      );
+    }
+  };
+
+  return {
+    addObject: addObject,
+    updateStore: updateStore,
+    addedObjects: addedObjects
+  };
 };
+
+var app = RenderStore();
 
 // update the cart as soon as the page loads!
 updateCart();
-updateStore();
+app.updateStore();
